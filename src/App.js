c819358862a75b131947b78972347c01b646e337
Brennan <bmtymrak@gmail.com>
2020-09-25 15:13:42 -0400

    Initial commit with functional prototype

--- src/App.js
+++ src/App.js
@@ -1,26 +1,116 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Card from './Card.js';
 import './App.css';
 
-function App() {
+function App({setup, createBoard}) {
+
+  const [activeTeam , setActiveTeam] = useState(setup[1])
+  const [cards, setCards] = useState(setup[0])
+  const [remainingCards, setRemainingCards] = useState(updateScore(setup[0]))
+  const [callerView, setCallerView] = useState(false)
+  const [canClick, setCanClick] = useState(true)
+  const [gameActive, setGameActive] = useState(true)
+
+  function updateScore(cards){
+      return (cards.reduce((teamRemaining, card)=>{
+      if (card.clicked === false){
+          if (card.team in teamRemaining){
+          teamRemaining[card.team]++
+          }else{
+              teamRemaining[card.team] = 1
+          }
+      }
+      return teamRemaining
+  },{})
+      )
+  }
+
+  function onTouch(card){
+      const newCards = [...cards]
+      const index = newCards.indexOf(card)
+      newCards[index].clicked = true
+      setCards(newCards)
+      const score = updateScore(newCards)
+      setRemainingCards(score)
+
+      if (!score[activeTeam]){
+          setGameActive(false)
+          setCanClick(false)
+          setCallerView(true)
+      }
+
+      else if(activeTeam !== card.team){
+          setCanClick(false)
+      }
+
+      if (card.team === 'assassin'){
+        handleEndOfGame()
+    
+      }
+  }
+
+  function handleEndOfGame(){
+    setGameActive(false)
+    const newActiveTeam = activeTeam === 'blue' ? 'red': 'blue'
+    setActiveTeam(newActiveTeam)
+    setCallerView(true)
+  }
+
+  function handleViewChange(){
+      setCallerView(prevCallerView => !callerView)
+  }
+
+  function handleCanClick(){
+      if (gameActive){
+      const newActiveTeam = activeTeam === 'blue' ? 'red': 'blue'
+      setActiveTeam(newActiveTeam)
+      setCanClick(true)
+      }
+  }
+
+  function newGame(){
+      let [cards, firstTeam] = createBoard()
+      setCards(cards)
+      setActiveTeam(firstTeam)
+      setRemainingCards(updateScore(cards))
+      setCanClick(true)
+      setGameActive(true)
+      setCallerView(false)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="container">
+          <h1>{gameActive ? `${activeTeam}'s turn` : `${activeTeam} wins!`}</h1>
+          <div className="score-container">
+              <div className="score blue">{remainingCards.blue ? remainingCards.blue : 0}</div>
+              <button onClick={handleCanClick}>
+                {activeTeam==='blue' ? 'End Blue turn' : 'End Red turn'}
+            </button>
+              <div className="score red">{remainingCards.red ? remainingCards.red : 0}</div>
+          </div>
+          <div className="controls-container">
+            <button onClick={handleViewChange}>
+            {callerView ? 'Switch to Player View' : 'Switch to Caller View'}
+            </button>
+            <button onClick = {newGame}>
+                New Game
+          </button>
+          </div>
+          <div className="board-container">
+          {cards.map((card)=>(
+              <Card 
+              key={card.word} 
+              card={card}
+              activeTeam={activeTeam}
+              onTouch={onTouch}
+              callerView={callerView}
+              canClick = {canClick}
+              colors={{'red': '#ea4f4f', 'blue': '#338bea', 'assassin': 'gray', 'none': '#979777'}}
+              colorsCaller={{'red': '#eebaba', 'blue': 'rgb(130, 188, 244)', 'assassin': '#c4c4c4'}}
+               />))}
+          </div>
+      </div>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,61 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+const wordList = ['africa', 'agent', 'air', 'alien', 'amazon', 'angel', 'antarctica', 'apple', 'arm', 'back', 'band', 'bank', 'bark', 'beach', 'belt', 'berlin', 'berry', 'board', 'bond', 'boom', 'bow', 'box', 'bug', 'canada', 'capital', 'cell', 'center', 'china', 'chocolate', 'circle', 'club', 'compound', 'copper', 'crash', 'cricket', 'cross', 'death', 'dice', 'dinosaur', 'doctor', 'dog', 'dress', 'dwarf', 'eagle', 'egypt', 'engine', 'england', 'europe', 'eye', 'fair', 'fall', 'fan', 'field', 'file', 'film', 'fish', 'flute', 'fly', 'forest', 'fork', 'france', 'gas', 'ghost', 'giant', 'glass', 'glove', 'gold', 'grass', 'greece', 'green', 'ham', 'head', 'himalaya', 'hole', 'hood', 'hook', 'human', 'horseshoe', 'hospital', 'hotel', 'ice', 'ice cream', 'india', 'iron', 'ivory', 'jam', 'jet', 'jupiter', 'kangaroo', 'ketchup', 'kid', 'king', 'kiwi', 'knife', 'knight', 'lab', 'lap', 'laser', 'lawyer', 'lead', 'lemon', 'limousine', 'lochness', 'log', 'mammoth', 'maple', 'march', 'mass', 'mercury', 'millionaire', 'model', 'mole', 'moscow', 'mouth', 'mug', 'needle', 'net', 'new york', 'night', 'note', 'novel', 'nurse', 'nut', 'oil', 'olive', 'olympus', 'opera', 'orange', 'paper', 'park', 'part', 'paste', 'phoenix', 'piano', 'telescope', 'teacher', 'switch', 'swing', 'sub', 'stick', 'staff', 'stadium', 'sprint', 'spike', 'snowman', 'slip', 'shot', 'shadow', 'server', 'ruler', 'row', 'rose', 'root', 'rome', 'rock', 'robot', 'robin', 'revolution', 'rat', 'racket', 'queen', 'press', 'port', 'pilot', 'time', 'tooth', 'tower', 'truck', 'triangle', 'trip', 'turkey', 'undertaker', 'unicorn', 'vacuum', 'van', 'wake', 'wall', 'war', 'washer', 'washington', 'water', 'wave', 'well', 'whale', 'whip', 'worm', 'yard']
+
+function createBoard(){
+    let boardStuff = []
+    let teams = []
+
+    let firstTeam = Math.random() < .5 ? 'red' : 'blue'
+    let secondTeam = firstTeam === 'red' ? 'blue' : 'red'
+
+    // Create list of teams
+    while(teams.length < 25){
+        teams.push('none')
+        }
+
+    teams.fill(firstTeam, 0, 9)
+    teams.fill(secondTeam, 9, 17)
+    teams[19] = 'assassin'
+
+    // Shuffle list of teams
+    let m = teams.length
+    let t, i;
+
+    while (m) {
+
+        i = Math.floor(Math.random() * m--);
+
+        t = teams[m];
+        teams[m] = teams[i];
+        teams[i] = t;
+    }
+
+    // Randomly select words from word list
+    while (boardStuff.length < 25){
+        let currWord = wordList[Math.floor(Math.random() * 187)]
+        if (boardStuff.indexOf(currWord) === -1){
+            boardStuff.push(currWord)
+        }
+    }
+
+    // Assign team to word
+    const words = boardStuff.map(word=>{
+        let index = boardStuff.indexOf(word)
+        return {'word': word, 'team': teams[index], 'clicked': false}
+    })
+
+    return [words, firstTeam]
+}
+
+let setup = createBoard()
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App setup={setup} createBoard={createBoard}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
